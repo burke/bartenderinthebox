c8b91f43c8b5e7c5437546b3540bf022562a1df5
Burke Libbey <burke@burkelibbey.org>
2008-10-24 20:55:49 -0500

    We'll be using Barduino for the actual hardware interface

--- lib/booze_monkey.rb
+++ lib/booze_monkey.rb
@@ -1,45 +1,37 @@
-require "#{BTB_ROOT}/lib/reservoir"
-
-require 'rubygems'
-require 'yaml'
-
 # The BoozeMonkey handles monitoring of reservoirs, and the mapping of ingredients to reservoirs.
 # BartenderInTheBox tells the BoozeMonkey it wants 30mL of vodka, and BoozeMonkey
 # activates the appropriate reservoir for the appropriate time period.
-# We might need to take viscosity into account :/
 class BoozeMonkey
 
   def initialize
     @reservoirs = YAML.load(File.open("#{BTB_ROOT}/data/reservoirs.yml"))
   end
 
+  # Dispense ingredients, all at once.
   def dispense( ingredients )
     threads = []
     ingredients.each do |id, quantity|
       threads << Thread.new do
         reservoir = reservoir_by_contents( id )
-        $log.debug "Activating Reservoir ##{reservoir.bay} for #{quantity} mL of #{reservoir.contents}."
+        $log.debug "Activating Reservoir ##{reservoir.bay} for #{quantity} mL of I##{reservoir.contents}."
         reservoir.dispense( quantity )
-        $log.debug "Finished dispensing #{reservoir.contents}"
+        $log.debug "Finished dispensing I##{reservoir.contents}"
       end
     end
     threads.map{ |t| t.join }
   end
 
   def inventory
-    ingredients = @reservoirs.map{ |res| res.contents }
-    return ingredients
+    @reservoirs.map{ |res| res.contents }
   end
 
-  def inventory_a
+  def reservoir_contents
     @reservoirs.map do |res|
       {res.bay => {
         :contents_id => res.contents,
         :contents_name => INGREDIENTS[res.contents][:name] }}
     end
   end
-
-
 
   private ###########################
 
@@ -51,9 +43,4 @@ class BoozeMonkey
     @reservoirs.select{ |res| res.bay == bay }.first
   end
 
-end
-
-if __FILE__ == $0 #############################
-  boozemonkey = BoozeMonkey.new
-  boozemonkey.dispense( 21, 30 )
 end
