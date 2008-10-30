f5d242c0761607776088bb7b0b381b9bff856b95
Burke Libbey <burke@burkelibbey.org>
2008-10-30 12:37:05 -0500

    Did I change anything?

--- models/drink.rb
+++ models/drink.rb
@@ -5,13 +5,13 @@ class Drink < ActiveRecord::Base
   has_many :ingredients, :through => :recipe_items
 
   def available?
-    inventory = Reservoir.find(:all).map{|r|r.id}
+    inventory = Reservoir.find(:all).map{|r|r.ingredient_id}
     (ingredients.map{|r|r.id} - inventory) == []
     # ingredients.all?{|i| i.available?}
   end
 
   def self.find_available
-    inventory = Reservoir.find(:all).map{|r|r.id}
+    inventory = Reservoir.find(:all).map{|r|r.ingredient_id}
 
     Drink.find(:all).select do |drink|
       (drink.ingredients.map{|r|r.id} - inventory) == []
