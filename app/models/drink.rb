4fcf1e249347336904dd0ec510a0de881306dcd0
Burke Libbey <burke@burkelibbey.org>
2010-12-09 16:32:08 -0600

    refactor

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -8,28 +8,21 @@ class Drink < ActiveRecord::Base
 
   scope :available, where(<<-SQL)
   id IN (
-
     -- All the drinks that have at least one ingredient present
-    SELECT d FROM (
-      SELECT DISTINCT drinks.id AS d
-      FROM drinks 
-      INNER JOIN recipe_items ON drinks.id = recipe_items.drink_id 
-      LEFT OUTER JOIN reservoirs ON recipe_items.ingredient_id = reservoirs.ingredient_id
-      WHERE reservoirs.id NOT NULL
+    SELECT DISTINCT d1.id
+    FROM drinks d1
+    INNER JOIN recipe_items ON d1.id = recipe_items.drink_id 
+    LEFT JOIN reservoirs ON recipe_items.ingredient_id = reservoirs.ingredient_id
+    WHERE reservoirs.id IS NOT NULL
+    AND d1.id NOT IN (
+      -- All the drinks that have at least one ingredient missing.
+      SELECT DISTINCT d2.id
+      FROM drinks d2
+      INNER JOIN recipe_items ri2 ON d2.id = ri2.drink_id 
+      LEFT JOIN reservoirs r2 ON ri2.ingredient_id = r2.ingredient_id
+      WHERE r2.id IS NULL
     )
-
-    EXCEPT 
-
-      -- All thr drinks that have at least one ingredient missing.
-      SELECT d2 FROM (
-        SELECT DISTINCT drinks.id AS d2
-        FROM drinks 
-        INNER JOIN recipe_items ON drinks.id = recipe_items.drink_id 
-        LEFT OUTER JOIN reservoirs ON recipe_items.ingredient_id = reservoirs.ingredient_id
-        WHERE reservoirs.id IS NULL
-      )
-    )
-  
+  )
   SQL
 
   def mix
